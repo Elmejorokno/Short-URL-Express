df81aa1d899c3e6d7648a037e5c785f8bf4af822
Ferdy Daniel Iglesias <ferdyiglesias12@gmail.com>
2022-03-12 11:39:30 -0600

    CRUD listo. redireccionamiento listo.

--- routes/home.js
+++ routes/home.js
@@ -3,14 +3,19 @@ const {
   leerUrls,
   agregarUrl,
   eliminarUrl,
+  editarUrlForm,
+  editarUrl,
+  redirectShortUrl,
 } = require('../Controllers/homeController')
+const checkUrl = require('../middlewares/checkUrl')
 
 const router = express.Router()
 
 router.get('/', leerUrls)
-
-router.post('/', agregarUrl)
-
+router.post('/', checkUrl, agregarUrl)
 router.get('/eliminar/:idUrl', eliminarUrl)
+router.get('/editar/:idUrl', editarUrlForm)
+router.post('/editar/:idUrl', checkUrl, editarUrl)
+router.get('/:shortUrl', redirectShortUrl)
 
 module.exports = router
